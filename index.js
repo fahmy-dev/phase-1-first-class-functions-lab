4dfccd29c2e058f9c758dc2da43e67bd60bd2e02
fahmy-dev <faahmy@proton.me>
2024-11-28 01:05:26 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function () {
+    const allDrivers =  (['Antonia', 'Nuru', 'Amari', 'Mo']);
+    return allDrivers.slice(0, 2); 
+}
+
+const returnLastTwoDrivers = function () {
+    const allDrivers =  (['Antonia', 'Nuru', 'Amari', 'Mo']);
+    return allDrivers.slice(2, 4); 
+}
+
+const selectingDrivers =  ([returnFirstTwoDrivers, returnLastTwoDrivers]);
+
+function createFareMultiplier(multiplier) {
+    return function (fare) {
+        return fare * multiplier;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driverFunction) {
+    return driverFunction(drivers);
+}
